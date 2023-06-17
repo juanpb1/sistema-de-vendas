e443ec477a33a2186f8b296b2432e2a7c60f65de
juanpb1 <jpprog1@gmail.com>
2023-06-17 11:59:41 -0300

    Funções de Ler, Atualizar e Deletar Produto

--- app/controllers/produto.hs
+++ app/controllers/produto.hs
@@ -2,23 +2,11 @@ module Controllers.Produto where
 
 import Model.Produto
 import Database.SQLite.Simple
-import qualified Data.Text as T
 import Database.SQLite.Simple.Types
 import Data.String (fromString)
-import Database.SQLite.Simple.FromRow
-import Database.SQLite.Simple.FromField
-import Database.SQLite.Simple.ToRow
-import Database.SQLite.Simple.ToField
-
---Faz a conversão entre os dados Haskell e o banco de dados
-instance FromRow Produto where
-    fromRow = Produto <$> field <*> field <*> field <*> field <*> field
-
-instance ToRow Produto where
-    toRow (Produto id' nome' marca' preco' quantidade') = toRow (id', nome', marca', preco', quantidade')
 
 
-addProduto :: Int -> String -> String -> Double -> Int -> IO ()
+addProduto :: Int -> String -> String -> Double -> Int -> IO()
 addProduto produtoId produtoNome marca preco quantidade = do
     conn <- open "app/db/sistemavendas.db"
     let query = fromString "INSERT INTO Produto (idProduto, nome, marca, preco, quantidade) VALUES (?, ?, ?, ?, ?)"
@@ -26,14 +14,33 @@ addProduto produtoId produtoNome marca preco quantidade = do
     print "Produto adicionado"
     close conn
 
-instance Show Produto where
-    show produto = mconcat [ show $ produtoId produto,
-                            ".) ",
-                            nomeProduto produto,
-                            "\nMarca: ",
-                            marca produto,
-                            "\nPreço: ",
-                            show $ preco produto,
-                            "\nQuantidade: ",
-                            show $ quantidade produto,
-                            "\n "]
+lerProdutos :: IO()
+lerProdutos = do 
+    conn <- open "app/db/sistemavendas.db"
+    let query = fromString "SELECT idProduto, nome, marca, preco, quantidade FROM Produto"
+    produtos <- query_ conn query :: IO[(Int, String, String, Double, Int)]
+    putStrLn "Produtos:"
+    mapM_ (\(produtoId, nome, marca, preco, quantidade) -> putStrLn $ "ID: " ++ show produtoId ++ 
+                                                                    "\nNome: " ++ show nome ++ 
+                                                                    "\nMarca: " ++ marca ++ 
+                                                                    "\nPreço: R$"++ show preco ++
+                                                                    "\nQuantidade: "++ show quantidade ++
+                                                                    "\n") produtos
+    close conn
+
+atualizarProduto :: Int -> String -> String -> Double -> Int -> IO()
+atualizarProduto produtoId produtoNome marca preco quantidade = do
+    conn <- open "app/db/sistemavendas.db"
+    let query = fromString "UPDATE Produto SET nome = ?, marca = ?, preco = ?, quantidade = ? WHERE idProduto = ?"
+    execute conn query (produtoNome, marca, preco, quantidade, produtoId)
+    putStrLn "Produto atualizado"
+    close conn
+
+
+deletarProduto :: Int -> IO()
+deletarProduto produtoId = do
+    conn <- open "app/db/sistemavendas.db"
+    let query = fromString "DELETE FROM Produto WHERE idProduto = ?"
+    execute conn query (Only produtoId)
+    putStrLn "Produto removido"
+    close conn
